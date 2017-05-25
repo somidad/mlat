6e737e2c719ecd1052cbb6009e4cda97901045a5
Seokseong Jeon <songsong@monet.postech.ac.kr>
2017-05-25 22:55:58 +0900

    Python: separate demo script from package

--- python/mlat/__init__.py
+++ python/mlat/__init__.py
@@ -68,39 +68,3 @@ class MLAT:
         idx = np.nanargmin(ret['error'])
         estimator = ret['estimator'][idx]
         return estimator, ret
-
-    @staticmethod
-    def demo():
-        W = 9
-        L = 9
-        H = 3
-        anchors = np.array([[0, 0, H],
-                            [W, 0, H],
-                            [W, L, H],
-                            [0, L, H]])
-        node = np.array([W * np.random.rand(),
-                         L * np.random.rand(),
-                         H * np.random.rand()])
-        ranges = np.empty(anchors.shape[0])
-        error = 0.5
-        ranges_with_error = np.empty(anchors.shape[0])
-        for i in range(anchors.shape[0]):
-            ranges[i] = np.linalg.norm(anchors[i, :] - node)
-            ranges_with_error[i] = ranges[i] + np.random.uniform(-error, error)
-
-        estimator, result = MLAT.mlat(anchors, ranges_with_error)
-
-        print('Anchors')
-        print(anchors)
-        print('Node:', node)
-        print('Ranges:')
-        print('   ', ranges)
-        print('Ranges with error:')
-        print('   ', ranges_with_error)
-        print('Estimator')
-        print(estimator)
-        print('Full result')
-        print(result)
-
-if __name__ == "__main__":
-    MLAT.demo()
